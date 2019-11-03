daa9f989a0b5af5d23ae695a62d40c3744ae43a3
BesLox <sup3r.h3r0.9x@gmail.com>
2019-11-03 10:42:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "secret": "s0me-secr3t-goes-here",
-    "refreshTokenSecret": "some-s3cret-refre2h-token",
+    "secret": "s3cr3t-t0k3n-1s-h3r3",
+    "refreshTokenSecret": "s3cr3t-refre2h-token",
     "port": 3000,
     "tokenLife": 900, // 15 minutes
     "refreshTokenLife": 86400 // a day
